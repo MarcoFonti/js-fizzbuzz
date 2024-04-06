5676bd82c62c6417e9f1829cc5929727f565ac8e
MarcoFonti <theemporiumg@gmail.com>
2024-04-06 15:48:10 +0200

    add commit

--- js/script.js
+++ js/script.js
@@ -1,55 +1,97 @@
 // Check
 console.log('JS OK');
 
-/*for (let i = 1; i <= 100; i++){
-    //console.log(i);
-    
+
+/* CICLO FOR DA 1 A 100 INCREMENTANDO DI UNO OGNI GIRO  */
+for (let i = 1; i <= 100; i++) {
+    /* CONTROLLO LA I */
+    console.log(i);
+
+
+    /* CREO UNA VARIBILE E GLI ASSEGNO LA I PER NANIPOLARLA NELLA CONDIZIONE */
     let element = i;
 
-    if(i % 15 === 0) {
+
+    /* SE MULTIPLO DI DI 15 */
+    if (i % 15 === 0) {
+        /* STAMPO */
         element = 'fizzbuzz';
+
+        /* ALTRIMENTI SE MULTIPLO DI 3 */
     } else if (i % 3 === 0) {
+
+        /* STAMPO */
         element = 'fizz';
+
+        /* ALTRIMENTI SE MULTIPLO DI 5 */
     } else if (i % 5 === 0) {
+
+        /* STAMPO */
         element = 'buzz';
-    } 
+    }
 
+
+    /* CONTROLLO IL RISULTATO */
     console.log(element);
-}*/
+}
 
 
-//Predndo Id 
+/* RECUPERO ELEMENTO ID IN HTML */
 const list = document.getElementById('recipe-numbers');
 
 
-// Creo la variabile in cui ci andro a scrivere 
+/* CREO UNA VARIBILE VUOTA PER ASSEGNARLA IN SEGUITO */
 let items = '';
 
-// Creo condizione
-for (let i = 1; i <= 100; i++){
-    
-    // Creo la variabile che nel caso cambi alla condizione
+
+/* CICLO FOR DA 1 A 100 INCREMENTANDO DI UNO OGNI GIRO  */
+for (let i = 1; i <= 100; i++) {
+
+
+    /* CREO UNA VARIBILE E GLI ASSEGNO LA I PER NANIPOLARLA NELLA CONDIZIONE */
     let element = i;
 
-    if(i % 3 === 0 && i % 5 === 0) {
+
+    /* SE MULTIPLO DI 3 E ANCHE 5  */
+    if (i % 3 === 0 && i % 5 === 0) {
+
+        /* STAMPO */
         element = 'fizzbuzz';
+
+        /* ALTRIMENTI SE MULTIPLO SOLO DI 3 */
     } else if (i % 3 === 0) {
+
+        /* STAMPO */
         element = 'fizz';
+
+        /* ALTRIMENTI SE MULTIPLO SOLO DI 5 */
     } else if (i % 5 === 0) {
+
+        /* STAMPO */
         element = 'buzz';
     }
 
+
+    /* CREO UNA VARIBILE VUOTA PER USARLA DENTRO A UNA CONDIZIONE */
     let className = '';
+
+
+    /* SE LA VARIBILE ELEMENT NON E' UN NUMERO */
     if (isNaN(element)) {
+
+        /* SE NON E' UN NUMERO LA METTO IN UNA VARIBILE */
         className = element;
     }
 
-    // Stampo in console
-    //console.log(element);
 
-    // Riempo la mia variabile
+    /* CONTROLLO IL RISULTATO */
+    console.log(element);
+
+
+    /* AGGIUNGO ALLA VARIBILE ITEMS UN TAG HTML HA CUI PASSO SPECIFICI VALORI RECUPERATI  */
     items += `<li class="${className}">${element}</li>`;
 }
 
-// Stampo in pagina
+
+/* STAMPO IN PAGINA */
 list.innerHTML = items;
